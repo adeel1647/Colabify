27332507a8ce37af05ae9bc901262b02dc077e8c
adeelahmed1647 <team164722@gmail.com>
2025-05-06 23:16:00 +0500

    updated

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-export const API_URL = "http://192.168.123.52:5000";
+//export const API_URL = "http://192.168.123.52:5000";
 
-//export const API_URL = "https://collabify-backend-production.up.railway.app";
+
+
+export const API_URL = "https://collabify-backend-production.up.railway.app";
